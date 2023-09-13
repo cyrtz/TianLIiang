4c2fbbb6f9e322dbcaaca51a12a7269d2c548a17
cyrtz <cys200302030@gmail.com>
2023-09-13 19:16:41 +0800

    add material

--- tianliang/src/app/app.module.ts
+++ tianliang/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { FAQComponent } from './faq/faq.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FAQComponent } from './faq/faq.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
