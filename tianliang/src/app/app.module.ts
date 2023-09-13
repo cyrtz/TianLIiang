b685e0d7c22aa1ca1ffdaec22c2ef878e1f31bc6
cyrtz <cys200302030@gmail.com>
2023-09-13 13:03:19 +0800

    建立常見問題元件

--- tianliang/src/app/app.module.ts
+++ tianliang/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { FAQComponent } from './faq/faq.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductComponent
+    ProductComponent,
+    FAQComponent
   ],
   imports: [
     BrowserModule,
