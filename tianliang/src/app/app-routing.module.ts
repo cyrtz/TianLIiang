7ecb0a381f9a992039a77a75f69504de23712228
cyrtz <cys200302030@gmail.com>
2023-09-13 18:32:44 +0800

    將元件放入導覽列

--- tianliang/src/app/app-routing.module.ts
+++ tianliang/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { FAQComponent } from './faq/faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', component: HomeComponent},
+  {path: '', redirectTo: '/product', component:ProductComponent},
+  {path: '', redirectTo: '/faq', component:FAQComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
